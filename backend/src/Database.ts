a7fbc847d45784dcb4cdaab70d22c313dcbad0d2
11ai <124818466+11ai@users.noreply.github.com>
2024-01-27 01:50:32 +0200

    edited on Fri Jan 26 2024 23:50:12 GMT+0000 (Coordinated Universal Time)

--- backend/src/Database.ts
+++ backend/src/Database.ts
@@ -24,7 +24,7 @@ export class Database {
     await Database.Seed();
   }
   static async Seed() {
-    let data: any = {"Books":[{"title":"The Great Gatsby","author":"F. Scott Fitzgerald","authorGender":"Male"},{"title":"To Kill a Mockingbird","author":"Harper Lee","authorGender":"Female"},{"title":"1984","author":"George Orwell","authorGender":"Male"}]};
+    let data: any = {"Books":[{"title":"To Kill a Mockingbird","author":"Harper Lee","authorGender":"Female","dateOfBirth":"1926-04-28"},{"title":"1984","author":"George Orwell","authorGender":"Male","dateOfBirth":"1903-06-25"},{"title":"The Great Gatsby","author":"F. Scott Fitzgerald","authorGender":"Male","dateOfBirth":"1896-09-24"}]};
     //Autogenerate multiple such calls ie for each resource and its data object
     let isSeeded = await this.IsSeeded();
     //if (!isSeeded) {
