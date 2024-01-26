8fe6906dafbd3433c2f7b9b5dd3963e406561409
11ai <124818466+11ai@users.noreply.github.com>
2024-01-27 01:50:22 +0200

    edited on Fri Jan 26 2024 23:50:11 GMT+0000 (Coordinated Universal Time)

--- frontend/src/data.ts
+++ frontend/src/data.ts
@@ -1,3 +1,3 @@
 
-let data: any = {"defaultData":{"Books":[{"title":"The Great Gatsby","author":"F. Scott Fitzgerald","authorGender":"Male"},{"title":"To Kill a Mockingbird","author":"Harper Lee","authorGender":"Female"},{"title":"1984","author":"George Orwell","authorGender":"Male"}]}};
+let data: any = {"defaultData":{"Books":[{"title":"To Kill a Mockingbird","author":"Harper Lee","authorGender":"Female","dateOfBirth":"1926-04-28"},{"title":"1984","author":"George Orwell","authorGender":"Male","dateOfBirth":"1903-06-25"},{"title":"The Great Gatsby","author":"F. Scott Fitzgerald","authorGender":"Male","dateOfBirth":"1896-09-24"}]}};
 export default data;
