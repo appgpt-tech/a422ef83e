10e5e44ecd8b93801b39a6777c0462afea5ea2f3
11ai <124818466+11ai@users.noreply.github.com>
2024-01-27 01:42:43 +0200

    created on Fri Jan 26 2024 23:42:32 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/dashboard.tsx
@@ -0,0 +1,7 @@
+import { Card, CardContent, CardHeader } from "@mui/material";
+    export const Dashboard = () => (
+      <Card>
+      <CardHeader title= "libby" />
+      <CardContent> A hobby project for tracking personal book readings < /CardContent>
+      </Card>
+    );
